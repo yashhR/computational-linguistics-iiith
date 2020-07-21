329688a2cc6fd27f864ad555a976863df3490194
yashhR <yashwanthasteroid@gmail.com>
2020-07-21 15:15:23 +0530

    Goal-11

--- RDIP/Codes/unit3.js
+++ RDIP/Codes/unit3.js
@@ -34,6 +34,41 @@
                              'I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌ ‌yesterday‌',
                              'I‌ ‌bought‌ ‌a‌ ‌book‌ ‌yesterday‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌',
                              'yesterday‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌'];
+            var engsent=['John ate an apple before afternoon',
+                            'before afternoon John ate an apple',
+                            'John before afternoon ate an apple',
+                            'some students like to study in the night',
+                            'at night some students like to study',
+                            'John and Mary went to church',
+                            'Mary and John went to church',
+                            'John went to church after eating',
+                            'after eating John went to church',
+                            'John after eating went to church',
+                            'did he go to market',
+                            'he did go to market',
+                            'the woman who called my sister sells cosmetics',
+                            'the woman who sells cosmetics called my sister',
+                            'my sister who sells cosmetics called the woman',
+                            'my sister who called the woman sells cosmetics',
+                            'John goes to the library and studies',
+                            'John studies and goes to the library',
+                            'John ate an apple so did she',
+                            'she ate an apple so did John',
+                            'the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌ ‌after‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌',
+                            'the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌ ‌after‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌',
+                            'after‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌',
+                            'after‌ ‌the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌',
+                            'she‌ ‌returned‌ ‌the‌ ‌book‌ ‌after‌ ‌the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌',
+                            'she‌ ‌noticed‌ ‌the‌ ‌error‌ ‌after‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌',
+                            'after‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌ ‌the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌',
+                            'after‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌',
+                            'I‌ ‌told‌ ‌her‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌yesterday‌',
+                             'I‌ ‌told‌ ‌her‌ ‌yesterday‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌',
+                             'yesterday‌ ‌I‌ ‌told‌ ‌her‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌',
+                             'I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌ ‌yesterday‌',
+                             'I‌ ‌bought‌ ‌a‌ ‌book‌ ‌yesterday‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌',
+                             'yesterday‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌']
+
                 var hin1 = ['राम‌ ‌और‌ ‌श्याम‌ ‌बाजार‌ ‌गयें‌',
                     'राम‌ ‌और‌ ‌श्याम‌ ‌गयें‌ ‌बाजार‌',
                     'बाजार‌ ‌गयें‌ ‌राम‌ ‌और‌ ‌श्याम‌',
@@ -151,6 +186,7 @@ function language()
             document.getElementById("choosenword").innerHTML ="";
             document.getElementById("reformsent").innerHTML = "";
             document.getElementById("checksent").innerHTML="";
+            document.getElementById("wordsjumble").innerHTML=" ";
             document.getElementById("correct").innerHTML="";
             document.getElementById("wrong").innerHTML="";
             document.getElementById("correctans").innerHTML="";
@@ -198,7 +234,7 @@ function language()
                 document.getElementById("choosenword").innerHTML ="";
                 document.getElementById("reformsent").innerHTML = "";
                 document.getElementById("checksent").innerHTML="";
-                document.getElementById("wordsjumble").innerHTML=" "
+                document.getElementById("wordsjumble").innerHTML=" ";
                 document.getElementById("correct").innerHTML="";
                 document.getElementById("wrong").innerHTML="";
                 document.getElementById("correctans").innerHTML="";
@@ -242,7 +278,7 @@ function language()
     else
         {
             document.getElementById("selectionintro").innerHTML="";
-            document.getElementById("introd").innerHTML=
+            document.getElementById("introd").innerHTML="";
             document.getElementById("formedsent").innerHTML="";
             document.getElementById("choosenword").innerHTML ="";
             document.getElementById("reformsent").innerHTML = "";
@@ -352,7 +388,7 @@ function language()
                 answers+="<p>"+randomEnglish[i]+"</p><br>";
             }   
         }
-        if(language=="hindi")
+        if(language==="hindi")
         {var i=0;
             for(i=0;i<randomHindi.length;i++)
             {
